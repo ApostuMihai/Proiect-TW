2f58498a10fefbb47bd94ab30811f7ee0c7160e6
ApostuMihai <82765760+ApostuMihai@users.noreply.github.com>
2022-12-08 17:14:05 +0200

    typo-uri

--- index.js
+++ index.js
@@ -5,19 +5,9 @@ app = express();
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/resurse", express.static(__dirname + "/resurse"));
-// app.get("/index", function (req, res) {
-//   // console.log("ceva");
-//   // res.send("Nu primesti");
-//   // res.sendFile(__dirname + "/index.html");
-//   res.render("pages/index");
-// });
 app.get(["/", "/index", "/home"], function (req, res) {
   res.render("pages/index", { ip: req.ip });
 });
-
-// app.get(["/despre"], function (req, res) {
-//   res.render("pagini/despre", { imagini: obGlobal.imagini });
-// });
 
 app.get("/*", function (req, res, next) {
   res.render("pages" + req.url, function (err, rezRandare) {
@@ -36,6 +26,7 @@ app.get("/*", function (req, res, next) {
     }
   });
 });
+
 obGlobal = {
   erori: null,
 };
@@ -51,7 +42,6 @@ function renderError(res, identificator, titlu, text, imagine) {
   var eroare = obGlobal.erori.info_erori.find(function (elem) {
     return elem.identificator == identificator;
   });
-
   titlu =
     titlu || (eroare && eroare.titlu) || obGlobal.erori.eroare_default.titlu;
   text = text || (eroare && eroare.text) || obGlobal.erori.eroare_default.text;
@@ -59,15 +49,15 @@ function renderError(res, identificator, titlu, text, imagine) {
     imagine ||
     (eroare && obGlobal.erori.cale_baza + "/" + eroare.imagine) ||
     obGlobal.erori.cale_baza + "/" + obGlobal.erori.eroare_default.imagine;
-
   if (eroare && eroare.status) {
     res
       .status(identificator)
-      .render("pages/eroare", { titlu: titlu, text: text, imagine: imagine });
+      .render("pagini/eroare", { titlu: titlu, text: text, imagine: imagine });
   } else {
-    res.render("pages/eroare", { titlu: titlu, text: text, imagine: imagine });
+    res.render("pagini/eroare", { titlu: titlu, text: text, imagine: imagine });
   }
 }
+
 app.get("/*ejs", function (req, res) {
   renderError(res, 403);
 });
